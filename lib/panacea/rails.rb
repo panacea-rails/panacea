9a7c273e52c41a0009ace2775414f91447d72d76
Eduardo Figarola <eduardofigarola@gmail.com>
2018-07-31 22:20:01 -0500

    Add Slop gem to make easier the handling of arguments

--- lib/panacea/rails.rb
+++ lib/panacea/rails.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
+require "slop"
 require "panacea/rails/version"
 
 module Panacea
   module Rails
-    # Your code goes here...
+    class << self
+      def init
+        opts = Slop.parse do |o|
+          o.banner = "usage: panacea your-app-name [options]"
+
+          o.string "-d", "--database", "options (mysql/postgresql/sqlite3/oracle/frontbase/im_db/sqlserver/jdbcmysql/jdbcsqlite3/jdbcpostgresql/jdbc)", default: "postgres"
+
+          o.on "-v", "--version" do
+            puts Panacea::Rails::VERSION
+            exit
+          end
+
+          o.on "-h", "--help" do
+            puts o
+            exit
+          end
+        end
+
+        puts opts
+      end
+    end
   end
 end
