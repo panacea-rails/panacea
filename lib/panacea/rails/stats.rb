53fee077973eb11b438fec91420223dc7695ca27
Guillermo Moreno <guillermo@michelada.io>
2018-08-28 20:21:15 +0000

    Ask if panacea user would like to share usage info

---
+++ lib/panacea/rails/stats.rb
@@ -0,0 +1,49 @@
+require "json"
+require "net/http"
+
+module Panacea
+  module Rails
+    class Stats
+      attr_reader :params
+
+      API_BASE = "https://panacea-stats.herokuapp.com/statistics"
+
+      def self.track(params)
+        new(params)
+      end
+
+      def initialize(params)
+        @params = params
+        track
+      end
+
+      def track
+        request_async_post(params)
+      end
+
+      private
+
+        def request_async_post(params)
+          Thread.new do
+            request_post(params)
+          end
+        end
+
+        def request_post(params)
+          uri = URI(API_BASE)
+          response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |http|
+            request = Net::HTTP::Post.new(uri)
+            request["Accept"] = "application/json"
+            request["Content-Type"] = "application/json"
+            request.body = params.to_json
+
+            http.request(request)
+          end
+
+          response.code == "201" || response.code != "422"
+        rescue Net::OpenTimeout, Errno::ECONNREFUSED => e
+          false
+        end
+    end
+  end
+end
